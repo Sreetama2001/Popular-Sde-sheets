bdbf70d0eab5180459982e8cec8e2ec77684d4b0
Sreetama <fool100abc@gmail.com>
2022-10-11 21:34:41 +0530

    Another sol of missing number

--- neetcode/268._Missing_Number.cpp
+++ neetcode/268._Missing_Number.cpp
@@ -1,4 +1,22 @@
 // easy 
 // Approach with Xor 
+/*
+Rules 
+a^a = 0
+a^0 = a
+*/
 
+// Xor with two arrays left out will be the missing one 
 
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        // xor
+        int res=nums.size();
+        for(int i =0;i<nums.size();i++){
+            res^=nums[i];
+            res^=i;
+        }
+        return res;
+    }
+};
