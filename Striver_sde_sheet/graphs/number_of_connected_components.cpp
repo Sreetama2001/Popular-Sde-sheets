9ed071a04da181cf9936ead2c486bf270d889ecd
Sreetama <fool100abc@gmail.com>
2022-10-19 22:53:28 +0530

    adding

---
+++ Striver_sde_sheet/graphs/number_of_connected_components.cpp
@@ -0,0 +1,9 @@
+// gfg question 
+// https://www.youtube.com/watch?v=lea-Wl_uWXY&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=5
+// question Given an undirected graph, the task is to print all the connected components line by line. 
+/*
+-> not connected graphs together is a graph 
+-> Maintain a visited array
+-> Then transverse all connected components and mark visited as true 
+cd 
+*/
---
+++ neetcode/DP/coin_change1.cpp
@@ -0,0 +1,9 @@
+// 322. Coin Change
+
+// DP mempization
+// neetcode 
+// Time complexity 
+// Space complexity
+
+// https://www.youtube.com/watch?v=H9bfqozjoqs
+
